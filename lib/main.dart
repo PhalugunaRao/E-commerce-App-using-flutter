a32442e2cdf0fb1255db6f489bee4642d69348a1
PhalgunaRao <phalu900@gmail.com>
2021-07-15 21:35:20 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    new MaterialApp(
+      home: new Container(
+        color: Colors.white,
+        child: new Text('Geethika'),
+      ),
+    )
+  );
+}
